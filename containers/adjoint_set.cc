9a572ce866e8008d0a9e608e4fd0d4dfa7aa8a1b
dacapo <dacapo1142@gmail.com>
2016-09-28 18:31:18 +0800

    fix linker issue

--- containers/adjoint_set.cc
+++ containers/adjoint_set.cc
@@ -1,5 +1,24 @@
 #include "adjoint_set.h"
 #include <iostream>
 
-template <typename T1, typename T2>
-AdjointSet::AdjointSet(size_t _n, size_t _k, T1 a, T2 b) : n(_n), k(_k) {}
+template <typename T>
+AdjointSet::AdjointSet(size_t n, size_t k, T which_cluster_begin,
+                       T which_cluster_end)
+    : _n(n), _k(k), data(n), first(k, NONE), last(k, NONE), size(k, 0),
+      which_cluster(which_cluster_begin, which_cluster_end) {
+    assert(_n != NONE);
+    initial();
+}
+
+void AdjointSet::print() {
+    for (size_t cid = 0; cid < _k; cid++) {
+        if (!empty(cid)) {
+            size_t vid = first[cid];
+            while (vid != NONE) {
+                std::cout << vid << " ";
+                vid = next(vid);
+            }
+            std::cout << std::endl;
+        }
+    }
+}
--- test.cc
+++ test.cc
@@ -1,9 +1,20 @@
 #include "containers/adjoint_set.h"
 #include <iostream>
+#include <typeinfo>
+#include <vector>
 using namespace std;
+template <typename T1> void test(T1 t1, T1 t2) {
+    for (T1 t = t1; t != t2; t++) {
+        cout << *t << " ";
+    }
+    cout << "\n";
+}
+
 int main() {
     int a[] = {1, 0, 1, 1, 2};
     AdjointSet s(5, 3, a, a + 5);
-    cout << s.n << endl;
+    s.print();
+    vector<int> v(a, a + 5);
+    test(a, a + 5);
     return 0;
 }
