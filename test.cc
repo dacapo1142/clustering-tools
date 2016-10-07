e9eb03a2d36df4b71fd0aca9ac7fa338400c5c4f
dacapo1142 <dacapo1142@gmail.com>
2016-10-08 02:14:48 +0800

    add test.cc

--- test.cc
+++ test.cc
@@ -3,18 +3,11 @@
 #include <typeinfo>
 #include <vector>
 using namespace std;
-template <typename T1> void test(T1 t1, T1 t2) {
-    for (T1 t = t1; t != t2; t++) {
-        cout << *t << " ";
-    }
-    cout << "\n";
-}
 
 int main() {
     int a[] = {1, 0, 1, 1, 2};
     DisjointSets s(5, 3, a, a + 5);
     s.print();
-    vector<int> v(a, a + 5);
-    test(a, a + 5);
+    cout << s.empty(0);
     return 0;
 }
