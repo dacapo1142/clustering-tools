947ba7e5dc4c7291b0b85b6eedd1953102a507c6
dacapo1142 <dacapo1142@gmail.com>
2017-10-17 20:55:39 +0800

    fix clear function

--- containers/VectorSet.hpp
+++ containers/VectorSet.hpp
@@ -3,51 +3,55 @@
 #include <cstddef>
 #include <limits>
 #include <vector>
+#include <iostream>
+using namespace std;
+
 class VectorSet {
   private:
-    std::vector<size_t> _list;
-    std::vector<size_t> _position;
+    std::vector<size_t> _data;
+    std::vector<size_t> _index;
     size_t _size;
     const static size_t NONE;
 
   public:
     VectorSet(size_t max_length)
-        : _list(max_length), _position(max_length, NONE), _size(0){};
+        : _data(max_length), _index(max_length, NONE), _size(0){};
     inline void insert(size_t vid) {
-        if (_position[vid] == NONE) {
-            _list[_size] = vid;
-            _position[vid] = _size;
+        if (_index[vid] == NONE) {
+            _data[_size] = vid;
+            _index[vid] = _size;
             _size++;
         }
         return;
     }
     inline void erase(size_t vid) {
-        if (_position[vid] != NONE) {
-            size_t vid_pos = _position[vid];
+        if (_index[vid] != NONE) {
+            size_t vid_pos = _index[vid];
             if (vid_pos != _size - 1) {
-                size_t last_val = _list[_size - 1];
-                std::swap(_list[_size - 1], _list[vid_pos]);
-                _position[last_val] = vid_pos;
+                size_t last_val = _data[_size - 1];
+                std::swap(_data[_size - 1], _data[vid_pos]);
+                _index[last_val] = vid_pos;
             }
-            _position[vid] = NONE;
+            _index[vid] = NONE;
             _size--;
         }
     }
-    inline auto begin() { return _list.begin(); }
-    inline auto end() { return _list.begin() + _size; }
+    inline auto begin() { return _data.begin(); }
+    inline auto end() { return _data.begin() + _size; }
     auto find(const size_t &vid) {
-        if (_position[vid] != NONE) {
-            return this->begin() + _position[vid];
+        if (_index[vid] != NONE) {
+            return this->begin() + _index[vid];
         } else {
             return this->end();
         }
     }
-    inline auto &operator[](size_t i) { return _list[i]; }
+    inline auto &operator[](size_t i) { return _data[i]; }
     inline size_t size() { return _size; }
     inline void clear() {
-        while (_size != 0) {
-            erase(_list[_size - 1]);
+        for(auto &v:*this){
+            _index[v]=NONE;
         }
+        _size=0;
     }
 };
 
