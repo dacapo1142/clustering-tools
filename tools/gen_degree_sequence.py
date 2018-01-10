a4136c159478763c6b805b8b260de441b26eefaf
dacapo1142 <dacapo1142@gmail.com>
2018-01-10 21:47:55 +0800

    rewrite gen sequence

--- tools/gen_degree_sequence.py
+++ tools/gen_degree_sequence.py
@@ -1,29 +1,20 @@
 import sys
 from collections import defaultdict
 
+
 pair_record = set()
 sequence = defaultdict(int)
 for line in sys.stdin:
-    try:
-        vid1, vid2 = map(int, line.split()[:2])
-    except:
-        continue
-    pair = frozenset({vid1, vid2})
-    if pair in pair_record:
-        continue
-    else:
-        pair_record.add(pair)
-        sequence[vid1]+=1
-        sequence[vid2]+=1
-
-
+    vid1, vid2 = map(int, line.split())
+    sequence[vid1]+=1
+    sequence[vid2]+=1
+    
 try:
     n = int(sys.argv[1])
     for v in range(n):
-        k = sequence.get(v, 0)
+        k = sequence[v]
         print('{} {}'.format(v, k))
 except:
-    items = sorted(sequence.items(), key=lambda x:x[0])
-    for v, k in items:
+    for v, k in sequence.items():
         print('{} {}'.format(v, k))        
     
